7c7d5f71143eaacdabfdee04d08dcae81d187fd0
Robert Åberg <robertab@kth.se>
2016-06-28 16:34:07 +0200

    Refactored most of the code

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,13 @@
+# from django.http import HttpResponse
 from django.shortcuts import render
 
 
-# Create your views here.
+# def home_page(request):
+#     if request.method == 'POST':
+#         return HttpResponse(request.POST['item_text'])
+#     return render(request, 'lists/home.html')
+
+
 def home_page(request):
-    return render(request, "lists/home.html")
+    context = {'new_item_text': request.POST.get('item_text', '')}
+    return render(request, 'lists/home.html', context)
